622d8c20108d3a87a4a33be52dba4e60b9edb1cd
shmelevsergei <shmelevsergei86@gmail.com>
2023-08-19 00:50:06 +0300

    0:49 19.08.2023

--- assets/scripts/Movement.ts
+++ assets/scripts/Movement.ts
@@ -1,53 +1,69 @@
-import { _decorator, CCInteger, Component, input, Input, KeyCode, Node, RigidBody, RigidBody2D, Vec2, Vec3 } from 'cc';
+import {
+	_decorator,
+	CCInteger,
+	Component,
+	input,
+	Input,
+	KeyCode,
+	Node,
+	RigidBody2D,
+	Vec2,
+	Vec3,
+} from "cc";
 const { ccclass, property } = _decorator;
 
-@ccclass('Movement')
+@ccclass("Movement")
 export class Movement extends Component {
-    @property(RigidBody2D)
-    rb : RigidBody2D;
-    @property(Node)
-    cameraNode : Node;
+	@property(RigidBody2D)
+	rb: RigidBody2D;
+	@property(Node)
+	cameraNode: Node;
 
-    horizontalMove : number = 0;
+	horizontalMove: number = 0;
 
-    @property(CCInteger)
-    speed : number = 1;
+	@property(CCInteger)
+	speed: number = 1;
 
-    onKeyDown(event) {
-        switch (event.keyCode) {
-            case KeyCode.ARROW_UP:
-                break;
-            case KeyCode.ARROW_RIGHT:
-                this.horizontalMove = 1;
-                break;
-            case KeyCode.ARROW_LEFT:
-                this.horizontalMove = -1;
-                break;
-        }
-    }
+	onKeyDown(event) {
+		switch (event.keyCode) {
+			case KeyCode.ARROW_UP:
+				break;
+			case KeyCode.ARROW_RIGHT:
+				this.horizontalMove = 1;
+				break;
+			case KeyCode.ARROW_LEFT:
+				this.horizontalMove = -1;
+				break;
+		}
+	}
 
-    onKeyUp(event) {
-        switch (event.keyCode) {
-            case KeyCode.ARROW_UP:
-                break;
-            case KeyCode.ARROW_RIGHT:
-                this.horizontalMove = 0;
-                break;
-            case KeyCode.ARROW_LEFT:
-                this.horizontalMove = 0;
-                break;
-        }
-    }
+	onKeyUp(event) {
+		switch (event.keyCode) {
+			case KeyCode.ARROW_UP:
+				break;
+			case KeyCode.ARROW_RIGHT:
+				this.horizontalMove = 0;
+				break;
+			case KeyCode.ARROW_LEFT:
+				this.horizontalMove = 0;
+				break;
+		}
+	}
 
-    start() {
-        input.on(Input.EventType.KEY_DOWN, this.onKeyDown, this);
-        input.on(Input.EventType.KEY_UP, this.onKeyUp, this);
-    }
+	start() {
+		input.on(Input.EventType.KEY_DOWN, this.onKeyDown, this);
+		input.on(Input.EventType.KEY_UP, this.onKeyUp, this);
+	}
 
-    protected update(dt: number): void {
-        this.rb.linearVelocity = new Vec2(this.horizontalMove * this.speed * dt, 0);
-        this.cameraNode.position = new Vec3(this.node.position.x, this.node.position.y + 200, 1000);
-    }
+	protected update(dt: number): void {
+		this.rb.linearVelocity = new Vec2(
+			this.horizontalMove * this.speed * dt,
+			0
+		);
+		this.cameraNode.position = new Vec3(
+			this.node.position.x,
+			this.node.position.y + 200,
+			1000
+		);
+	}
 }
-
-
